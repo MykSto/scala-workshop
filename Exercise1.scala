bfa082189bc7639cc974ec648051eff318257430
MykSto <hushokas@gmail.com>
2017-09-07 14:15:54 +0300

    this is sparta

---
+++ Exercise1.scala
@@ -0,0 +1,7 @@
+//Exercise1.scala
+val number = 17
+number = 20// reassignment to this value cannot be proceded
+val value = "ABC1234"
+value = "DEF1234" // reassignment to this value cannot be proceded
+val double = 15.56
+println(number,value,double)
---
+++ Values.scala
@@ -0,0 +1,9 @@
+//Values.scala
+val number = 4
+val word = "originalGang"
+val double = 3.2
+
+println(number, word, double)
+/*Output:
+(4, originalGang, 3.2)
+*/
