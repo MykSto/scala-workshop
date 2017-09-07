b431e7791b19e3449e88e32e268f118f2567f4ba
MykSto <hushokas@gmail.com>
2017-09-07 22:06:25 +0300

    Data types
    practicing data types

---
+++ DataType.scala
@@ -0,0 +1,14 @@
+//DataType.scala
+val number:Int = 5
+val letNum:String = "ABC1234"
+val double:Double = 5.4
+val boolean:Boolean = true
+val multiple:String = """If I ever meet you
+we shall share a drink
+or two"""
+val smth:Boolean = "maybe" //DataType mismatch
+val comaNum:Int = 15.4 //DataType mismatch
+val svNum:Double = 11 //DataType mismatch, but readable
+println(number, letNum, double, boolean)
+println(multiple)
+println(smth, comaNum, svNum)
