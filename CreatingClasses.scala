bdbabc59a0fde51b7a1b1421f858ebffe570a6b5
MykSto <hushokas@gmail.com>
2017-09-12 23:27:33 +0300

    Simple Classes

---
+++ CreatingClasses.scala
@@ -0,0 +1,30 @@
+//CreatingClasses.scala
+class Hippo
+class Lion
+class Tiger
+class Monkey
+class Giraffe 
+
+val h = new Hippo
+val l = new Lion
+val l2 = new Lion
+val t = new Tiger
+val m = new Monkey
+val g1 = new Giraffe
+val g2 = new Giraffe
+
+println(h)
+println(l)
+println(l2)
+println(t)
+println(m)
+println(g1)
+println(g2)
+
+class Zebra {
+	println("I have stripes")
+}
+
+val zebra = new Zebra
+
+
